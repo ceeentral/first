47e3898cde6269cbab446c722c636186b9002bbc
chuan <chuanye2nd@gmail>
2018-06-05 22:45:15 +0800

    haha

--- python_practice/rock_paper_scissors.py
+++ python_practice/rock_paper_scissors.py
@@ -1,22 +1,18 @@
 __author__ = '3tral'
 
 print "welcome to the hotest ROCK PAPER SCISSORS all over the world"
-#p1 = ''
-#p2 = ''
+
 def startgame():
 	global p1
 	p1 = raw_input("Player1,please input your gesture: \nr for ROCK,\np for PAPER,\ns for SCISSORS\n")
 	global p2
 
 	p2 = raw_input("Player2,please input your gesture: \nr for ROCK,\np for PAPER,\ns for SCISSORS\n")
-	#return pp1, pp2
 
-	#p1 = a1
-	#p2 = a2
-def rackpaperscissors(para1, para2):
+def rockpaperscissors(para1, para2):
 	if para1 == 'r':
 		if para2 == 'r':
-			print "you play even"
+			print "draw"
 			a = raw_input("start a new game? (y/n)")
 			if a == 'y':
 				print 'hah, idiot.'
@@ -31,7 +27,7 @@ def rackpaperscissors(para1, para2):
 		if para2 == 'r':
 			print "player2 win!"
 		if para2 == 's':
-			print "you play even"
+			print "draw"
 			a = raw_input("start a new game? (y/n)")
 			if a == 'y':
 				startgame()
@@ -45,18 +41,21 @@ def rackpaperscissors(para1, para2):
 		if para2 == 's':
 			print "player2 win!"
 		if para2 == 'p':
-			print "you play even"
+			print "draw"
 			a = raw_input("start a new game? (y/n)")
 			if a == 'y':
 				startgame()
 			else:
 				pass
 				
-#p1, p2= startgame(p1, p2)
 startgame()
 print p1
 print p2
-rackpaperscissors(p1, p2)
+rockpaperscissors(p1, p2)
+
+# or another way
+# def racks():
+#     
 
 #https://www.practicepython.org/exercise/2014/03/26/08-rock-paper-scissors.html
 
