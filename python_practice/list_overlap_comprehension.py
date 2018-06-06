7de6c1f0cf8e57bd1b8921e6f8769e33b5cf088a
chuan <chuanye2nd@gmail>
2018-06-06 22:44:18 +0800

    compact my code.

---
+++ python_practice/list_overlap_comprehension.py
@@ -0,0 +1,5 @@
+
+__author__ = '3tral'
+
+
+#https://www.practicepython.org/exercise/2014/04/10/10-list-overlap-comprehensions.html
--- python_practice/test.py
+++ python_practice/test.py
@@ -1,17 +1,10 @@
-p1 = raw_input("what's your request?\n\n").lower()
-p2 = raw_input("what's your request?\n\n").lower()
-
-choices = list(['papper', 'rock', 'scissors'])
-
-if p1 not in choices:
-	print("you a goof")
-if p2 not in choices:
-	print("you a goof")
-if p1 == p2:
-	print("it's a draw")
-if choices.index(p1) == (choices.index(p2) + 1 ) % 3:
-	print ("player 2 wins!")
-if choices.index(p2) == (choices.index(p1) + 1 ) % 3:
-	print ("player 1 wins!")
-print choices.index(p1)
-print choices.index(p2)
+a = raw_input("input a number:\n")
+i = 5
+print ("tried %d" %i)
+if a == 'exit':
+	exit('user end the game.')
+else:
+	if int(a) not in range(1,5):
+		print "goof"
+	else:
+		print "genius"
