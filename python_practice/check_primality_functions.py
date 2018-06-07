b8209603511bf306e435c0cb573eacc2289b6b6a
chuan <chuanye2nd@gmail>
2018-06-07 22:40:21 +0800

    not clever but working

--- python_practice/check_primality_functions.py
+++ python_practice/check_primality_functions.py
@@ -1,8 +1,18 @@
 __author__ = '3tral'
 
-
-
-
+num = raw_input('please input a number:\n')
+if num.isdigit():
+	l1 = []
+	for i in range(2, int(num)):
+		if int(num) % i == 0 :
+			l1.append(i)
+	
+	if l1 == []:
+		print (str(num) + " is a prime number!")
+	else:
+		print (str(num) + " is NOT a prime number!")
+else:
+	print "we need a number!"
 
 
 
