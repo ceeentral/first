051f2dbd28871e429a1989bff95a62217cb68ab2
chuan <chuanye2nd@gmail>
2018-06-11 23:37:40 +0800

    fibo

--- python_practice/fibonacci.py
+++ python_practice/fibonacci.py
@@ -1,10 +1,17 @@
 __author__ = '3tral'
-num = raw_input("please give a number of numbers for Fibonacci:\n")
+num = int(raw_input("please give a number of numbers for Fibonacci:(should more than 1)\n"))
 
 def fibonacci(nums):
     a = [1, 1]
-    print a 
-    for(i=2;i<nums;i++):
-        a[i] = a[i-1] + a[i-2]
-        a.append(a[i])
+    if nums == 2:
+    	print a 
+    else:
+    	for i in range(2, nums):
+    	    c = a[i-1] + a[i-2]
+    	    a.append(c)
     print a
+fibonacci(num)
+
+
+
+#https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
