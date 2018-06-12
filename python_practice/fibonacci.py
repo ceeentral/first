4901fce8bbd023eead0a134e7e6a913cf6197a20
chuan <chuanye2nd@gmail>
2018-06-12 23:27:33 +0800

    add official answer

--- python_practice/fibonacci.py
+++ python_practice/fibonacci.py
@@ -18,4 +18,20 @@ def fibonacci(nums):
 
 fibonacci(getnum())
 
+#official answer: just like my answer
+#def gen_fib():
+#	count = int(raw_input("how many fibonacci numbers would you like to generate?"))
+#	i = 1
+#	if count == 0:
+#		fib[]
+#	elif count == 1:
+#		fib = [1]
+#	elif count == 2:
+#		fib = [1, 1]
+#	elif count > 2:
+#		fib = [1,1]
+#		while i < (count - 1):
+#			fib.append(fib[i] + fib[i-1])
+#			i +=1
+#	return fib
 #https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
