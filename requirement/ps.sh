f02cb6b7820549099071e3115a40aa838d7a82a0
Wang <central.wang@nokia-sbell.com>
2018-06-16 16:13:33 +0800

    ps

--- requirement/ps.sh
+++ requirement/ps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-oamadd=192.168.253.1:/root/
+
 day=$(date| awk '{print $2$3}')
 tm=$(date| awk '{print $4}'| awk -F: '{print $1$2}')
 lgps=${day}slave${tm}.log
 
-bsh 1
-journalctl -ab>${lgps}
-scp ${lgps} ${oamadd}
-#first logic journal grab
+bsh 1 "journalctl -ab>${lgps} && scp -o StrictHostKeyChecking=no ${lgps} toor4nsn@192.168.253.1:/tmp/ && rm ${lgps} && exit "
+ssh toor4nsn@192.168.253.20
 
