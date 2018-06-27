64958620d7180481c00ba3ebb8708b84a9726678
Wang <central.wang@nokia-sbell.com>
2018-06-27 09:44:12 +0800

    sth

--- requirement/ps.sh
+++ requirement/ps.sh
@@ -5,6 +5,7 @@ day=$(date| awk '{print $2$3}')
 tm=$(date| awk '{print $4}'| awk -F: '{print $1$2}')
 lgps=${day}slave${tm}.log
 
+journalctl -ab>/tmp/${day}hi${tm}.log
 bsh 1 "journalctl -ab>${lgps} && scp -o StrictHostKeyChecking=no ${lgps} toor4nsn@192.168.253.1:/tmp/ && rm ${lgps} && exit "
 ssh toor4nsn@192.168.253.20
 
---
+++ requirement/sshtoabik.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh toor4nsn@192.168.253.20 
