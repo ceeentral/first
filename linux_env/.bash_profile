9b81fd9e290ba8848fa85c77421b37af8f43ff11
chuan <chuanye2nd@gmail.com>
2018-07-01 02:19:31 +0800

    ii

---
+++ linux_env/.bash_profile
@@ -0,0 +1,10 @@
+#in normal linux, it names .bashrc, in Mac it call .bash_profile
+#在mac中是可有可无的，并且只针对单个用户来说，我～目录下的.bash_profile
+#就是我后来添加的，为了加一些快捷键alias以及一些环境变量export
+
+
+
+alias ll="ls -al"
+alias ls='ls -G'
+#export LSCOLORS=exfxaxdxcxegedabagacad
+alias grep='grep --color' 
---
+++ requirement/l1check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####rq=`date + "%m%d"`
+####sj=`date + "%H%M"`
+###day=$(date|awk '{print $2$3}')
+###tm=$(date|awk '{print $$4}' |awk -F: '{print $1$2}')
+###pfr=${day}${tm}fr.pcap
+###ssh cpif-0.local "sudo tcpdump -i fronthaul -c 10 -w ${pfr} -vv && scp ${pfr} oam-0.local:/home/robot/"
+
+value=`ssh toor4nsn@192.168.253.20 "rexxxxx"
+
+
