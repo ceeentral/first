e17f7e3e6ffccf7ff82913d25e84dbaf409981cf
chuan <chuanye2nd@gmail.com>
2018-07-02 08:28:47 +0800

    oo

---
+++ pye/test.py
@@ -0,0 +1,8 @@
+def HaveFun():
+    if __name__ == '__main__':
+        print('I am in my domain,my name is %s' % __name__)
+    else:
+        print('Someone else calls me!,my name is %s' % __name__)
+
+
+HaveFun()
---
+++ python_practice/cows_bulls.py
@@ -0,0 +1,53 @@
+__author__ = '3tral'
+import random
+def asser():
+
+	Rightvalue = random.randint(1000,9999)
+	print(Rightvalue)
+	l1 = list(str(Rightvalue))
+	print(l1)
+	userValue = urinput()
+	l2 = list(str(userValue))
+	guesstimeC = 0
+	cowCounter = 0
+	bullCounter = 0
+	cc = [i for i in l2 for x in l1 if i == x]
+	bullCounter = len(cc)
+	for i in range(0,4):
+		if l2[i] == l1[i]:
+			cowCounter+=1
+	if cowCounter > 0:
+		bullCounter = bullCounter - cowCounter
+	print('cow:' + str(cowCounter) + ' bull:' + str(bullCounter))
+	if cowCounter != 4:
+		aaa
+
+def urinput():
+	userValue = input("please input a 4-digits number\n")
+	return userValue
+def rannum():
+	Rightvalue = random.randint(1000,9999)
+	print(Rightvalue)
+	return Rightvalue
+
+def asser1(r1, r2):
+	l1 =list(str(r1))
+	l2 =list(str(r2))
+	l3 =[]
+	bull = [i for i in l2 for x in l1 if 1 == x]
+	if i not in l3:
+		l3.append[i]
+	bullCounter = len(l3)
+	for i in range(0,4):
+		if l2[i] == l1[i]:
+			cowCounter += 1
+	if cowCounter > 0:
+		bullCounter = bullCounter - cowCounter
+	return cowCounter, bullCounter
+if __name__ == __main__:
+	l1=[]
+	l1=asser1(rannum(),urinput())
+	print(l1)
+
+
+#https://www.practicepython.org/exercise/2014/07/05/18-cows-and-bulls.html
