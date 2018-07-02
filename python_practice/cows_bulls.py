4fc95345cdfb0138d0e5919a780a2d127ab2719e
chuan <chuanye2nd@gmail.com>
2018-07-02 08:45:55 +0800

    o1

--- python_practice/cows_bulls.py
+++ python_practice/cows_bulls.py
@@ -34,19 +34,22 @@ def asser1(r1, r2):
 	l1 =list(str(r1))
 	l2 =list(str(r2))
 	l3 =[]
+	cowCounter = 0
 	bull = [i for i in l2 for x in l1 if 1 == x]
-	if i not in l3:
-		l3.append[i]
+	e = [l3.append[i] for i in bull if i not in l3]
 	bullCounter = len(l3)
 	for i in range(0,4):
 		if l2[i] == l1[i]:
 			cowCounter += 1
 	if cowCounter > 0:
 		bullCounter = bullCounter - cowCounter
+	print(cowCounter, bullCounter)
 	return cowCounter, bullCounter
-if __name__ == __main__:
+if __name__ == "__main__":
 	l1=[]
 	l1=asser1(rannum(),urinput())
+	if l1[0] != 4:
+		print('aaa')
 	print(l1)
 
 
