f85770d60e9c2086fb74df0fe3af49de6c3a6d2b
Wang <central.wang@nokia-sbell.com>
2018-07-04 15:28:34 +0800

    delete again

--- python_practice/decode_page.py
+++
@@ -1,4 +0,0 @@
-__author__ = '3tral'
-#right now is too hard for me.
-
-#https://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
--- python_practice/odd_or_even.py
+++
@@ -1,9 +0,0 @@
-num = raw_input("please reinput your number: ")
-if (int(num) % 2 == 0):
-	if(int(num)%4 ==0):
-		print( str(num) + " is a multiple of 4")
-	else:
-		print ( str(num) + " is even")
-else:
-	print (str(num) + " is odd")
-
