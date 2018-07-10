642445e4326f330db3a1e3dc0cd06b6661471adf
Wang <central.wang@nokia-sbell.com>
2018-07-10 16:25:45 +0800

    ii

--- requirement/delog.sh
+++ requirement/delog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /ap128_gnb166/wts
-rm -rf egate.log* enb_1.log* mme_1.log* pgw_1.log* sgw_1.log* stdout_* uec_1.log* egate.log*
+cd ap128_gnb166/wts
+rm -rf egate.log* enb_1.log* mme_1.log* pgw_1.log* sgw_1.log* stdout_* uec_1.log* egate.log* error_*
 
 cd ../uec
 rm -rf egate.log*  stdout_*
