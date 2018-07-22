adb20ea4f0bf69ec24666d6a5a1f2f98c17f1e34
Wang <central.wang@nokia-sbell.com>
2018-07-22 21:48:10 +0800

    i22

---
+++ python_practice/22_readfromfile.py
@@ -0,0 +1,12 @@
+__author__ = '3tral'
+with open('RM.txt', 'r') as open_file:
+	all_text = open_file.read()
+	print all_text
+
+
+
+
+
+#reading file official web https://docs.python.org/3.3/tutorial/inputoutput.html#reading-and-writing-files
+
+#https://www.practicepython.org/exercise/2014/12/06/22-read-from-file.html
