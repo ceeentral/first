265994d1a881246facf5c6e51f105002c20cbeb7
Wang <central.wang@nokia-sbell.com>
2018-08-09 23:02:43 +0800

    for emu use

--- requirement/delog.sh
+++ requirement/delog.sh
@@ -5,3 +5,14 @@ rm -rf egate.log* enb_1.log* mme_1.log* pgw_1.log* sgw_1.log* stdout_* uec_1.log
 
 cd ../uec
 rm -rf egate.log*  stdout_*
+
+
+function proc(){
+	val1=`ps -ef |grep edaemon|sed -n '1p'|awk 'print{$2}'`
+	val2=`ps -ef |grep egate|sed -n '1p'|awk 'print{$2}'`
+	val3=`ps -ef |grep edaemon|sed -n '2p'|awk 'print{$2}'`
+	sudo kill $val1 $val2 $val3
+	
+}
+
+proc
