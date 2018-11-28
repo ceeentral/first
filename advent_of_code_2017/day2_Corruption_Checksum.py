fa654d22005147ce661c445ad4ddd309fe4bf7d3
Wang <central.wang@nokia-sbell.com>
2018-11-28 18:25:16 +0800

    update

--- advent_of_code_2017/day2_Corruption_Checksum.py
+++ advent_of_code_2017/day2_Corruption_Checksum.py
@@ -2,7 +2,27 @@
 #day 2 Corruption Checksum
 #_authour_=chuan
 
-arA = ('''414	382	1515	319	83	1327	116	391	101	749	1388	1046	1427	105	1341	1590
+def Array(lines):
+    "Parse an iterable of str lines into a 2-D array. If `lines` is a str, splitlines."
+    if isinstance(lines, str): lines = lines.splitlines()
+    return mapt(Vector, lines)
+def mapt(fn, *args): 
+    "Do a map, and make the results into a tuple."
+    return tuple(map(fn, *args))
+def Vector(line):
+    "Parse a str into a tuple of atoms (numbers or str tokens)."
+    return mapt(Atom, line.replace(',', ' ').split())
+
+def Atom(token):
+    "Parse a str token into a number, or leave it as a str."
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return token
+arA = Array('''414	382	1515	319	83	1327	116	391	101	749	1388	1046	1427	105	1341	1590
 960	930	192	147	932	621	1139	198	865	820	597	165	232	417	19	183
 3379	987	190	3844	1245	1503	3151	3349	2844	4033	175	3625	3565	179	3938	184
 116	51	32	155	102	92	65	42	48	91	74	69	52	89	20	143
@@ -18,19 +38,16 @@ arA = ('''414	382	1515	319	83	1327	116	391	101	749	1388	1046	1427	105	1341	1590
 334	275	395	128	347	118	353	281	430	156	312	386	160	194	63	141
 146	1116	153	815	2212	2070	599	3018	2640	47	125	2292	165	2348	2694	184
 1704	2194	1753	146	2063	1668	1280	615	163	190	2269	1856	150	158	2250	2459''')
-if isinstance(arA, str): 
-	arA = arA.splitlines()
-print(arA)
-arA = str(arA)
-arA = arA.replace('\\t', ' ').split(',')
 
-print(arA)
-count = 0
-for row in arA:
-	aa = max(row)
-	count = count + 1
-	print(count)
-	print(aa)
+
+#print(arA[0][3])
+#count = 0
+#for row in arA:
+#	print(row[9])
+#	count = count + 1
+#	print(count)
 	#count = sum(max(row)-min(row))
-print(count)
+#print(count)
 #print(arA.splitlines())
+
+print(arA[1][0])
